f32353b49d8e7e6e7fb7b8ca1dc003e8fa666aef
zeonbh <bharathr1993@gmail.com>
2017-05-13 18:50:56 -0400

    Add files via upload
    Added Board Class

---
+++ TicTacToe/src/tictactoe/Board.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tictactoe;
+
+/**
+ *
+ * @author Bharath
+ */
+public class Board {
+    
+    private char[][] board = new char[2][2];
+    public Board()
+    {
+        
+    }
+}
